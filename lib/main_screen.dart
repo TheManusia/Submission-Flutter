c06c5b84339c9158394f3dfad5f06dde5ad2302d
Ian <minecraftindo0@gmail.com>
2020-07-06 18:33:16 +0700

    Add Bottom Navigation Bar and Screen

---
+++ lib/main_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:submission_flutter/information_screen.dart';
+import 'package:submission_flutter/report_screen.dart';
+
+import 'home_screen.dart';
+import 'stats_screen.dart';
+
+class MainScreen extends StatefulWidget {
+  MainScreen({Key key}) : super(key: key);
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  static List<Widget> _screen = <Widget>[
+    HomeScreen(),
+    StatsScreen(),
+    ReportScreen(),
+    InformationScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _screen.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 0.0,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.insert_chart),
+            title: Text('Stats'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.assignment),
+            title: Text('School'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.info),
+            title: Text('Information'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.blueGrey,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
---
+++ lib/report_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ReportScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('Report');
+  }
+}
